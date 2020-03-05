4451c4bd7daa3b3c2fa9b70b20ef33abe2c322ae
rb-hslu <rbeutler#git config --global user.email rbeutler@gmx.chgit config --global user.email rbeutler#>
2020-03-05 20:21:18 +0100

    Add excercise 02b

---
+++ 00_Exercises/02b_multiTableDesc/multiTableDesc.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printUsageMessage(void);
+void printMultiTable(int);
+
+const int lowerLimit = 1;
+const int upperLimit = 20;
+
+
+int main(int argc, char* argv[])
+{
+	int inputValue = 0;
+
+	if(argc != 2)
+	{
+		// no valid input number
+		printUsageMessage();
+		return 0;
+	}
+	else
+	{
+		inputValue = atoi(argv[1]);
+		
+		if(inputValue >= lowerLimit && inputValue <= upperLimit)
+		{
+			// input is valid
+			printMultiTable(inputValue);
+		}
+		else
+		{
+			// no valid input
+			printUsageMessage();
+			return 0;
+		}
+	}
+	
+	return 0;
+}
+
+
+
+void printUsageMessage(void)
+{
+	printf("Usage: multiTable wholeNumber\n\twholeNumber: integer on the interval[lowerLimit,upperLimit]\n");
+}
+
+void printMultiTable(int tableLimit)
+{
+	for(int i = 1; i <= tableLimit; i++)
+	{
+		for(int j = tableLimit; j >= 1; j--)
+		{
+			printf("%d\t",i*j);
+		}
+		printf("\n");
+	}
+}
+
+
